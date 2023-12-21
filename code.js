fd8a9501564a174e019864ce1146cc010b6335ff
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2023-12-21 13:56:54 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,67 +1,92 @@
 // Övning 1: Destructurera objekt
 const person = {
-  name: "Alice",
-  age: 30,
-  address: "123 Main St",
-};
+    name: "Alice",
+    age: 30,
+    address: "123 Main St",
+  };
+  
+  // Använd destructuering för att ta ut varje property till separat variabel
+  // TODO
+  
+  console.log(`Name: ${name}, Age: ${age}, Address: ${address}`); // Detta ska skrivas ut
+  
+  
+  // Övning 2: Destructurera arrayer
+  const colors = ["red", "green", "blue"];
+  
+  // Tilldela varje färg en variabel
+  // TODO
+  
+  console.log(color1, color2, color3); // Detta ska skrivas ut
+  
+  
+  // Övning 3: Använda spread operator (...)
+  const numbers1 = [1, 2, 3];
+  const numbers2 = [4, 5, 6];
+  
+  // Kompinera de båda arrayerna med spreadoperatorn
+  // TODO: 
+  
+  console.log(combinedNumbers); // Detta ska skrivas ut
+  
+  // Övning 4: Använda spread operator (...) för att uppdatera ett objekt
+  const product = {
+    id: 1,
+    name: "Apple",
+    price: 10,
+  };
+  
+  // Skapa ett nytt objekt där property "price" uppdateras till 15
+  // TODO
 
-// Använd destructuering för att ta ut varje property till separat variabel
-// TODO: 
-console.log(`Name: ${name}, Age: ${age}, Address: ${address}`); // Detta ska skrivas ut
+  console.log(updatedProduct); // Detta ska skrivas ut
+  
+  
+  // Övning 5: Rest parametern (...)
+  // Skapa en funktion som tar emot ett varierbart antal argument och returnerar summan av dem
+  // TODO
+  
+  console.log(sum(1, 2, 3, 4, 5)); // Ska ex skriva ut 15
+  
+  
+  // Övning 6: Destructurera nästlade objekt
+  const user = {
+    id: 1,
+    info: {
+      name: "John",
+      email: "john@example.com",
+    },
+  };
+  
+  // Extrahera alla properties från objektet user
+  // TODO
 
-// Övning 2: Destructurera arrayer
-const colors = ["red", "green", "blue"];
-
-// Tilldela varhe färg en variabel
-// TODO: 
-console.log(color1, color2, color3); // Detta ska skrivas ut
-
-// Övning 3: Spread operator
-const numbers1 = [1, 2, 3];
-const numbers2 = [4, 5, 6];
-
-// Kompinera de båda arrayerna med spreadoperatorn
-// TODO: 
-console.log(combinedNumbers); // Detta ska skrivas ut
-
-// Övning 4: Rest operatorn
-// Skapa en funktion som tar emot ett varierbart antal argument och returnerar summan av dem
-// TODO: 
-
-console.log(sum(1, 2, 3, 4, 5)); // Ska ex skriva ut 15
-
-// Övning 5: Destructurera nästlade objekt
-const user = {
-  id: 1,
-  info: {
-    name: "John",
-    email: "john@example.com",
-  },
-};
-
-// Extrahera alla properties från objektet user
-// TODO: 
-console.log(`User ID: ${id}, Name: ${name}, Email: ${email}`); // Detta ska skrivas ut
-
-// Övning 6: Destructurera nästlade arrayer
-const teams = [
-  { name: "Team A", players: ["Player 1", "Player 2"] },
-  { name: "Team B", players: ["Player 3", "Player 4"] },
-];
-
-// Extrahera alla properties från den nästlade arrayen.
-console.log(`Team Name: ${teamName}`); // Detta ska skrivas ut
-console.log(`Players: ${player1}, ${player2}`); // Detta ska skrivas ut
-
-// Övning 7 Rest-operatorn i objekt destructurering
-const book = {
-  title: "The Great Gatsby",
-  author: "F. Scott Fitzgerald",
-  year: 1925,
-  pages: 180,
-};
-
-// Destructurera så att title extraheras i en variabel och bookInfo till återstående
-const { title, ...bookInfo } = book;
-console.log(`Title: ${title}`); // Detta ska skrivas ut
-console.log("Book Info:", bookInfo); // Resten av objektet ska skrivas ut
+  console.log(`User ID: ${id}, Name: ${name}, Email: ${email}`); // Detta ska skrivas ut
+  
+  
+  // Övning 7: Destructurera nästlade arrayer
+  const teams = [
+    { name: "Team A", players: ["Player 1", "Player 2"] },
+    { name: "Team B", players: ["Player 3", "Player 4"] },
+  ];
+  
+  // Extrahera alla properties från den nästlade arrayen.
+  // TODO
+  
+  console.log(`Team Name: ${teamName}`); // Detta ska skrivas ut
+  console.log(`Players: ${player1}, ${player2}`); // Detta ska skrivas ut
+  
+  // Övning 8 Rest parametern i objekt destructurering
+  const book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    pages: 180,
+  };
+  
+  // Destructurera så att title extraheras i en variabel och bookInfo till återstående
+  // TODO
+  
+  console.log(`Title: ${title}`); // Detta ska skrivas ut
+  console.log("Book Info:", bookInfo); // Resten av objektet ska skrivas ut
+  
